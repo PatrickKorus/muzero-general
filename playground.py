bc2ab001c24d3774fb3773991aeeb2d70e4a5cd8
Patrick Korus <korus@gmx.net>
2020-08-10 09:05:04 +0200

    - added playground

---
+++ playground.py
@@ -0,0 +1,17 @@
+import gym
+
+from muzero import MuZero
+
+
+#env = gym.make("MountainCar-v0")
+
+#for it in range(2000):
+#    if it == 0 or done:
+#        env.reset()
+#    a = env.action_space.sample()
+#    env.render()
+#    o, r, done, _ = env.step(a)
+#env.close()
+muzero = MuZero("pendulums")
+muzero.train()
+muzero.test(render=True, opponent="self", muzero_player=None)
