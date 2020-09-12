09a78259e60bdcceda8a46728e89c656dbe06f12
Patrick Korus <korus@gmx.net>
2020-09-12 15:25:03 +0200

    - fixes in mountaincar

---
+++ playground.py
@@ -0,0 +1,21 @@
+import gym
+
+from muzero import MuZero
+
+env = gym.make('MountainCar-v0')
+r = 0
+env.reset()
+for it in range(1000):
+    action = env.action_space.sample()
+    _, reward, done, _ = env.step(action)
+    r += 0.995 * reward
+    if done:
+        break
+print(r)
+
+
+muzero = MuZero('mountaincar')
+
+muzero.train()
+
+muzero.test(render=True)
