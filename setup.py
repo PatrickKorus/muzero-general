7e6808cc3ca9cca6a076d805acb483258fcb001e
Patrick Korus <korus@gmx.net>
2020-10-06 02:59:19 +0200

    - general refactoring for external installation and use via setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mcts-general',
+    version='1.0.0',
+    packages=['muzero_general', 'games'],
+    url='https://github.com/PatrickKorus/muzero-general',
+    license='MIT',
+    author='Patrick Korus',
+    author_email='patrick.korus@stud.tu-darmstadt.de',
+    description='MuZero General Fork for Thesis Experiments'
+)
